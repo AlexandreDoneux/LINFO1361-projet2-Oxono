32a165df0dfa63187b9a058834257d2c549d69cd
AlexandreDoneux <94830560+AlexandreDoneux@users.noreply.github.com>
2026-04-12 21:20:01 +0200

    Add idées for heuristics/techniques/methods

--- random_agent.py
+++ random_agent.py
@@ -17,7 +17,12 @@ class RandomAgent(Agent):
 # idées d'heuristiques et techniques
 # - éviter de placer des pièces d'un type (x ou o) trop loin de celles déjà placées. On doit en aligner 4 don on évite
 # d'avoir un espace de 3 ou plus entre la pièce d'un type et les autres.
-# - laisser un temps plus en moins long en fonction du temps restant, du nombre de pièces déjà passées et de la valeur de la fonction d'évaluation de l'état a
-#
-#
+# - laisser un temps plus en moins long en fonction du temps restant, du nombre de pièces déjà passées et de la valeur de la fonction d'évaluation de l'état
+# - optimiser le code python pour gagner du temps à l'exécution (ex: éviter les boucles for imbriquées, utiliser des structures de données plus efficaces, etc.)
+# => gain de temps général et recherche o-plus éfficace
+# - faire des recherches sur un nombre limité de coups en avance (ex: 2 ou 3) et utiliser une fonction d'évaluation pour estimer la valeur de l'état à la fin de la recherche.
+# Cela permet de gagner du temps en évitant de rechercher jusqu'à la fin du jeu, tout en ayant une estimation de la valeur de l'état.
+#    => à utiliser dès le début du projet pour éviter de perdre du temps à implémenter une recherche jusqu'à la fin du jeu, ce qui est très coûteux en temps de calcul.
+# - améliorer les fonction d'évaluation pour qu'elles soient plus précises et prennent en compte plus de facteurs (ex: nombre de pièces alignées, nombre de pièces bloquées, etc.)
+#       => pour le moment audune idée...
 #
