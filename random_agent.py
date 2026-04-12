a67e89fccbbfa473d6213ec5e564eb59a4324ecc
AlexandreDoneux <94830560+AlexandreDoneux@users.noreply.github.com>
2026-04-12 19:10:23 +0200

    Adding multiple ideas of improvement in comments

--- random_agent.py
+++ random_agent.py
@@ -9,3 +9,15 @@ class RandomAgent(Agent):
     def act(self, state, remaining_time):
         actions = list(Game.actions(state))
         return random.choice(actions)
+
+
+
+
+
+# idées d'heuristiques et techniques
+# - éviter de placer des pièces d'un type (x ou o) trop loin de celles déjà placées. On doit en aligner 4 don on évite
+# d'avoir un espace de 3 ou plus entre la pièce d'un type et les autres.
+# - laisser un temps plus en moins long en fonction du temps restant, du nombre de pièces déjà passées et de la valeur de la fonction d'évaluation de l'état a
+#
+#
+#
