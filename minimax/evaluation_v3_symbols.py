40ef5982aa4a0bca1de955d9344b519f3d129006
Matthias Cicoea <cioceamatthias@gmail.com>
2026-04-19 16:35:25 +0200

    modification de la fonction evaluation et ajout d'une fonction pour traiter les symboles dans l'heurestie

---
+++ minimax/evaluation_v3_symbols.py
@@ -0,0 +1,247 @@
+from agent import Agent
+from oxono import Game
+
+
+# Depth limit for Minimax search.
+SEARCH_DEPTH = 2 # -> peut être ajusté. Faire comparaisons entre différentes valeurs pour un compromis qualité - vitesse ?
+# améliorations : ajustement dynamique en fonction du temps restant, du nombre de pièces déjà passées, de la valeur de la fonction d'évaluation, ...
+
+
+class MinimaxAgent(Agent):
+    """
+    Oxono agent using the Minimax algorithm. Based on the pseudo-code from the lectures. Available in
+    "Artificial intelligence : a modern approach" by Russell and Norvig, fourth edition, chapter 6, page 196.
+
+    The algorithm explores the game tree up to SEARCH_DEPTH, then give scores to non-terminal states with a heuristic evaluation function.
+    """
+
+    def __init__(self, player):
+        super().__init__(player)
+        self.windows = []
+        
+        # Fenêtres Horizontales (6 lignes * 3 positions par ligne = 18)
+        for r in range(6):
+            for c in range(3):
+                self.windows.append([(r, c + i) for i in range(4)])
+        
+        # Fenêtres Verticales (6 colonnes * 3 positions par colonne = 18)
+        for c in range(6):
+            for r in range(3):
+                self.windows.append([(r + i, c) for i in range(4)])
+
+
+    def act(self, state, remaining_time):
+        """
+        from the pseudo-code:
+            player<- game.TO-MOVE(state) => needed ? -> No because we are always MAX
+            value, move <- MAX-VALUE(game,state)
+            return move
+
+        Parameters
+        ----------
+        state          : current game state
+        remaining_time : seconds left on our clock for the whole game -> need to be used
+
+        Returns
+        -------
+        The best action found by Minimax at depth SEARCH_DEPTH.
+        """
+        # always start as MAX ? -> yes because we are the ones choosing the move
+        depth = 0 # to track depth
+        global SEARCH_DEPTH
+        SEARCH_DEPTH = adapt_depth(state, remaining_time)
+        _, best_move = self.max_value(state, depth)
+        return best_move
+
+
+    def max_value(self, state, depth):
+        """
+        from the pseudocode:
+
+            if game.IS-TERMINAL(state) then return game.UTILITY(state, player), null
+            v, move <- −∞
+            for each a in game.ACTIONS(state) do
+                v2, a2 <- MIN-VALUE(game, game.RESULT(state, a))
+                if v2 > v then
+                    v, move <- v2, a
+            return v, move
+
+        We add a depth limit: when depth reaches 0 on a non-terminal state,
+        we call the heuristic instead of recursing further.
+
+        Returns
+        -------
+        (score, action) : best score achievable and the move that leads to it.
+                            action is None at terminal/leaf nodes.
+        """
+
+
+        # if terminal state: return true utility
+        if Game.is_terminal(state):
+            return Game.utility(state, self.player), None
+
+        # if depth limit reached: estimate with heuristic
+        if depth == SEARCH_DEPTH:
+            return self.evaluate(state, self.player), None
+
+        # else: look for the highest score among possible actions
+        v = float('-inf')
+        best_move = None
+
+        for action in Game.actions(state):
+            next_state = state.copy()
+            Game.apply(next_state, action)
+
+            # Recurse into MIN
+            v2, _ = self.min_value(next_state, depth + 1)
+
+            # Keep track of the best score
+            if v2 > v:
+                v = v2
+                best_move = action
+
+        return v, best_move
+
+
+    def min_value(self, state, depth):
+        """
+        from the pseud-ocode:
+
+            if game.IS-TERMINAL(state) then return game.UTILITY(state, player), null
+            v, move <- +∞
+            for each a in game.ACTIONS(state) do
+                v2, a2 <- MAX-VALUE(game, game.RESULT(state, a))
+                if v2 < v then
+                    v, move <- v2, a
+            return v, move
+
+        Returns
+        -------
+        (score, action) : lowest score the opponent can force and the move
+                        that achieves it. action is None at terminal/leaf nodes.
+        """
+        # if terminal state: return true utility
+        if Game.is_terminal(state):
+            return Game.utility(state, self.player), None
+
+        # if depth limit reached: estimate with heuristic
+        if depth == SEARCH_DEPTH:
+            return self.evaluate(state, self.player), None
+
+        # else
+        v = float('+inf')
+        best_move = None
+
+        for action in Game.actions(state):
+            next_state = state.copy()
+            Game.apply(next_state, action)
+
+            # Recurse into MAX
+            v2, _ = self.max_value(next_state, depth + 1)
+
+            # Keep track of the best score
+            if v2 < v:
+                v = v2
+                best_move = action
+
+        return v, best_move
+    
+    # Estime la valeur d'un plateau non terminal en additionnant les scores de toutes les fenêtres de 4 cases pour les couleurs et symboles.
+    def evaluate(self, state, player):
+        opponent = 1 - player
+        score = 0
+        current_player = Game.to_move(state)
+        
+        # On récupère la grille
+        board = state.board
+
+        for window_coords in self.windows:
+            # Extraction de la fenêtre
+            window = [board[r][c] for r, c in window_coords]
+
+            # Evaluation en fonction des clouleurs + bonnus pour celui qui joue le prochain
+            mult_p = 2.0 if current_player == player else 1.0
+            mult_o = 2.0 if current_player == opponent else 1.0
+            
+            score += score_color_window(window, player, +1) * mult_p
+            score += score_color_window(window, opponent, -1) * mult_o
+
+            # Evaluation en fonction des symboles
+            for symbol in ['X', 'O']:
+                # On donne un score de base pour le symbole présent
+                symb_val = score_symbol_window(window, symbol)
+                
+                # Si le symbole appartient à une ligne presque finie, 
+                # le danger dépend de qui peut déplacer le totem correspondant.
+                score += symb_val * (1 if current_player == player else -1)# Evaluation en fonction des symboles
+
+        return score
+
+
+def adapt_depth(state, remaining_time):
+    "Ajustement dynamique mais avec des valeurs arbitraires pour l'instant, à améliorer"
+
+    played = number_of_plays(state)
+    print("adapt depth : ", played, remaining_time)
+
+    # si presque plus de temps, on doit pas perdre de temps
+    if remaining_time < 10:
+        return 2
+
+    # assez tôt, beaucoup de possibilités, on doit pas perdre de temps à explorer trop profondément
+    if played < 10:
+        print("early game")
+        return 3
+
+    # une fois plus de pièces posées, moins de possibilités. On peut rechercher plus en profondeur
+    if played < 24:
+        return 4
+
+    # vers la fin : peu de possibilités, on peut se permettre de rechercher plus en profondeur pour trouver le meilleur coup
+    if played > 30 :
+        return 6
+
+    return 5
+
+
+def number_of_plays(state):
+    return sum(1 for row in state.board for cell in row if cell is not None)
+
+
+# Attribue un score à une fenêtre selon le nombre de symboles (X ou O) présents.
+def score_color_window(window, color_player, sign):
+    # Poids de plus en plus élevées pour privilégier la victoire immédiate
+    weights = {4: 100, 3: 10, 2: 1}
+    opponent = 1 - color_player
+
+    my_count = 0
+    empty_count = 0
+    
+    for cell in window:
+        if cell is not None:
+            if cell[1] == color_player:
+                my_count += 1
+            else:
+                # Présence de l'adversaire : fenêtre bloquée pour la couleur
+                return 0
+        else:
+            empty_count += 1
+
+    # Si la ligne est déjà de 4, c'est une victoire 
+    if my_count == 4: return sign * 1000 
+    
+    # On ne score que si la ligne est complétable (pas de blocage adverse)
+    return sign * weights.get(my_count, 0)
+
+# Attribue un score à une fenêtre selon le nombre de symboles (X ou O) présents.
+def score_symbol_window(window, symbol):
+    # Les symboles ont moins de poids car tout le monde peut gagner avec
+    weights = {4: 50, 3: 5, 2: 0.5}
+    count = 0
+    
+    for cell in window:
+        if cell is not None and cell[0] == symbol:
+            count += 1
+            weights.get(count, 0)
+
+    return weights.get(count, 0)  # le 2eme argument est la si jamais get ne trouve pas la valeur count dans le dico
