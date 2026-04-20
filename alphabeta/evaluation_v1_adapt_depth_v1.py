8c499f9321873245390e1d6c46e806611c7ad229
Matthias Cicoea <cioceamatthias@gmail.com>
2026-04-20 18:23:04 +0200

    alpha-beta amélioré avec le cotrole du centre

--- alphabeta/evaluation_v1_adapt_depth_v1.py
+++ alphabeta/evaluation_v1_adapt_depth_v1.py
@@ -1,9 +1,7 @@
 from agent import Agent
 from oxono import Game
 
-
 # Depth limit for Alpha-Beta search.
-# Alpha-Beta prunes des branches -> on peut se permettre d'explorer plus profondément que minimax
 SEARCH_DEPTH = 4
 
 
@@ -26,143 +24,138 @@ class AlphaBetaAgent(Agent):
 
     def __init__(self, player):
         super().__init__(player)
-
+        
+        # initialisation de la liste de colones et lignes de 4
+        self.game = Game()
+        self.windows = []
+        for r in range(6):
+            for c in range(3):
+                self.windows.append([(r, c + i) for i in range(4)])
+        for c in range(6):
+            for r in range(3):
+                self.windows.append([(r + i, c) for i in range(4)])
 
     def act(self, state, remaining_time):
-        """
-        from the pseudo-code:
-            player <- game.TO-MOVE(state)
-            value, move <- MAX-VALUE(game, state, -∞, +∞)
-            return move
 
-        Parameters
-        ----------
-        state          : current game state
-        remaining_time : seconds left on our clock for the whole game
+        # On récupère la profondeur max 
+        depth_limit = adapt_depth(state, remaining_time)
+        
+        best_move = None
+        max_val = float('-inf')
+        alpha = float('-inf')
+        beta = float('inf')
 
-        Returns
-        -------
-        The best action found by Alpha-Beta at depth SEARCH_DEPTH.
-        """
-
-        depth = 0
-        global SEARCH_DEPTH
-        SEARCH_DEPTH = adapt_depth(state, remaining_time)
-        _, best_move = self.max_value(state, depth, alpha=float('-inf'), beta=float('+inf')) # alpha and beta initialosed
+        # boucle sur tous les coups possibles
+        for action in self.game.actions(state):
+            next_state = state.copy()
+            self.game.apply(next_state, action)
+            
+            # On lance l'algorithme alpha-beta pour chaque coup
+            val = self.alphabeta(next_state, depth_limit - 1, alpha, beta, False)
+            
+            # si un coup a une meilleur évaluation que best_move, on remplace best_move par ce coup
+            if val > max_val:
+                max_val = val
+                best_move = action
+            
+            alpha = max(alpha, val) # on met a jour la borne si on a un meilleur coup
+            
         return best_move
 
+    def alphabeta(self, state, depth, alpha, beta, maximizingPlayer):
+        
+        # condition d'arret
+        if depth == 0 or self.game.is_terminal(state):
+            if self.game.is_terminal(state):
+                return self.game.utility(state, self.player) * 100000 # si jamais on gagne ou perds il faut un énorme poids
+            # Ajout de self.player comme argument attendu
+            return self.evaluate(state, self.player) 
 
-    def max_value(self, state, depth, alpha, beta):
-        """
-        from the pseudo-code:
+        # c'est à nous de jouer
+        if maximizingPlayer:
+            maxEval = float('-inf')
+            for action in self.game.actions(state):
+                # on joue le coup
+                next_state = state.copy()
+                self.game.apply(next_state, action)
+                
+                eval = self.alphabeta(next_state, depth - 1, alpha, beta, False)
+                
+                maxEval = max(maxEval, eval)
+                alpha = max(alpha, eval)
+                if beta <= alpha:
+                    break 
+            return maxEval
 
-            if game.IS-TERMINAL(state) then return game.UTILITY(state, player), null
-            v, move <- −∞
-            for each a in game.ACTIONS(state) do
-                v2, a2 <- MIN-VALUE(game, game.RESULT(state, a), alpha, beta)
-                if v2 > v then
-                    v, move <- v2, a
-                    alpha <- MAX(alpha, v)
-                if v >= beta then return v, move   ← PRUNING: MIN will never allow this
-            return v, move
+        # c'est à l'adversaire de jouer 
+        else:
+            minEval = float('inf')
+            for action in self.game.actions(state):
+                next_state = state.copy()
+                self.game.apply(next_state, action)
+                
+                eval = self.alphabeta(next_state, depth - 1, alpha, beta, True)
+                
+                minEval = min(minEval, eval)
+                beta = min(beta, eval)
+                if beta <= alpha:
+                    break 
+            return minEval
+        
+    # Estime la valeur d'un plateau non terminal.
+    def evaluate(self, state, player):
+        opponent = 1 - player
+        score = 0
+        current_to_move = self.game.to_move(state)
+        board = state.board
+        plays = number_of_plays(state)
 
-        Parameters
-        ----------
-        state : current game state
-        depth : current depth in the tree (incremented at each recursive call)
-        alpha : best score MAX is already guaranteed on the path to the root
-        beta  : best score MIN is already guaranteed on the path to the root
+        """partie controle du centre"""
+        # au début, avoir des pieces au centre est plus avantageuses
+        POSITION_WEIGHTS = [
+            [1, 1, 1, 1, 1, 1],
+            [1, 2, 2, 2, 2, 1],
+            [1, 2, 3, 3, 2, 1],
+            [1, 2, 3, 3, 2, 1],
+            [1, 2, 2, 2, 2, 1],
+            [1, 1, 1, 1, 1, 1]
+        ]
+        
+        # fin de la partie --> avoir des lignes > placer les pieces au centre
+        center_multiplier = max(0.0, (15.0 - plays) / 15.0) # voir si 15 est un bon nombre ??
 
-        Returns
-        -------
-        (score, action) : best score achievable and the move that leads to it.
-                          action is None at terminal/leaf nodes.
-        """
-        if Game.is_terminal(state):
-            return Game.utility(state, self.player), None
+        for r in range(6):
+            for c in range(6):
+                cell = board[r][c]
+                if cell is not None:
+                    # cell[1] contient l'ID du joueur (0 ou 1)
+                    if cell[1] == player:
+                        score += POSITION_WEIGHTS[r][c] * center_multiplier * 3.0
+                    else:
+                        score -= POSITION_WEIGHTS[r][c] * center_multiplier * 3.0
 
-        if depth == SEARCH_DEPTH:
-            return evaluate(state, self.player), None
+        """partie lignes de 4"""
 
-        v = float('-inf')
-        best_move = None
+        for window_coords in self.windows:
+            # on transforme la liste de coordonnées en liste de cell 
+            window = [board[r][c] for r, c in window_coords]
 
-        for action in Game.actions(state):
-            next_state = state.copy()
-            Game.apply(next_state, action)
+            # score_color_window attribue un score a une fenetre selon combien de pieces de la couleur sont dedans 
+            val_p = score_color_window(window, player, +1)
+            val_o = score_color_window(window, opponent, -1)
 
-            v2, _ = self.min_value(next_state, depth + 1, alpha, beta)
+            # plus élevé pour le joueur qui va jouer au prochain tour
+            mult_p = 2.0 if current_to_move == player else 1.0
+            mult_o = 2.0 if current_to_move == opponent else 1.0
+            score += val_p * mult_p
+            score += val_o * mult_o
 
-            # Keep track of the best score
-            if v2 > v:
-                v = v2
-                best_move = action
+            # pour les windows de symboles
+            for symbol in ['X', 'O']:
+                symb_val = score_symbol_window(window, symbol)
+                score += symb_val * (1 if current_to_move == player else -1)
 
-            # Update alpha
-            alpha = max(alpha, v)
-
-            # Puning
-            if v >= beta:
-                return v, best_move
-
-        return v, best_move
-
-
-    def min_value(self, state, depth, alpha, beta):
-        """
-        from the pseudo-code:
-
-            if game.IS-TERMINAL(state) then return game.UTILITY(state, player), null
-            v, move <- +∞
-            for each a in game.ACTIONS(state) do
-                v2, a2 <- MAX-VALUE(game, game.RESULT(state, a), alpha, beta)
-                if v2 < v then
-                    v, move <- v2, a
-                    beta <- MIN(beta, v)
-                if v <= alpha then return v, move   ← PRUNING: MAX will never choose this
-            return v, move
-
-        Parameters
-        ----------
-        state : current game state
-        depth : current depth in the tree (incremented at each recursive call)
-        alpha : best score MAX is already guaranteed on the path to the root
-        beta  : best score MIN is already guaranteed on the path to the root
-
-        Returns
-        -------
-        (score, action) : lowest score the opponent can force and the move
-                          that achieves it. action is None at terminal/leaf nodes.
-        """
-        if Game.is_terminal(state):
-            return Game.utility(state, self.player), None
-
-        if depth == SEARCH_DEPTH:
-            return evaluate(state, self.player), None
-
-        v = float('+inf')
-        best_move = None
-
-        for action in Game.actions(state):
-            next_state = state.copy()
-            Game.apply(next_state, action)
-
-            v2, _ = self.max_value(next_state, depth + 1, alpha, beta)
-
-            # Keep track of the best score
-            if v2 < v:
-                v = v2
-                best_move = action
-
-            # Update beta
-            beta = min(beta, v)
-
-            # Pruning
-            if v <= alpha:
-                return v, best_move
-
-        return v, best_move
-
+        return score
 
 def adapt_depth(state, remaining_time):
     "Ajustement dynamique mais avec des valeurs arbitraires pour l'instant, à améliorer"
@@ -181,87 +174,48 @@ def adapt_depth(state, remaining_time):
 
     # une fois plus de pièces posées, moins de possibilités. On peut rechercher plus en profondeur
     if played < 24:
-        return 5
+        return 4
 
     # vers la fin : peu de possibilités, on peut se permettre de rechercher plus en profondeur pour trouver le meilleur coup
     if played > 30 :
-        return 7
+        return 8
 
     return 6
 
 
 def number_of_plays(state):
-
     return sum(1 for row in state.board for cell in row if cell is not None)
 
-
-def evaluate(state, player):
-    opponent = 1 - player
-    score = 0
-
-    windows = get_windows(state.board)
-
-    for window in windows:
-        score += score_color_window(window, player, +1)
-        score += score_color_window(window, opponent, -1)
-
-    return score
-
-
-
-# warning : but : aligner 4 pièces du même symbole où de la même couleur, mais aussi empêcher l'adversaire de le faire
-
-# Quelques règles/idées que j'ai trouvé :
-
-# Éviter les longues lignes de la couleur de l'adversaire : Vu que seul l'adversaire peut poser sa couleur faut éviter
-# les longues lignes dans sa couleur. Si il y a des lignes de 3 en sa couleur qui peuvent encore être complétés
-# => pas bon du tout, on doit diminuer fortement la valeur de cet état. Idem pour les lignes de deux à moindre mesure.
-# On doit aussi prendre en compte les lignes pouvant être complétées par le centre (genre [Black] [vide] [Black] [Black] est très dangereux).
-#
-# À l'inverse privilégier les lignes de notre couleur (en suivant les mêmes règles). On augmentera le score de l'état dans ces cas là.
-
-# Pour l'alignement de symboles nos pièces peuvent servir à l'adversaire et inversement. Donc l'évaluation de ce qui est
-# bien ou non est plus compliqué. Je n'ai pas encore trouvé quelque chose d'intéressant pour ça.
-
-# Les règles concernant un totem bloqué peut être intéressant vu qu'on limites les choix mais ça peut être à notre
-# avantage comme à l'avantage de l'adversaire. Je pense que ça peut être intéressant d'y regarder.
-
-
-def get_windows(board):
-    """
-    Return all horizontal and vertical slices of 4 consecutive cells on the board
-    """
-    windows = []
-
-    # horizontal
-    for r in range(6):
-        for c in range(3):
-            windows.append([board[r][c + i] for i in range(4)])
-    # vertical
-    for c in range(6):
-        for r in range(3):
-            windows.append([board[r + i][c] for i in range(4)])
-
-    return windows
-
-
+# Attribue un score à une fenêtre selon le nombre de pieces de la bonne (mauvaise) couleur présentse.
 def score_color_window(window, color_player, sign):
-    """
-    Score window of 4 cells (does not matter if vertical or horizontal, they are passed as 4 values) for a player's color.
-    Gives a score to a window that can be completed (no pieces of the opponent's color).
-    """
-    weights = {3: 0.8, 2: 0.1}
-    # mettre un poids plus lourd sur le gain ou la perte => orienter l'agent vers la mise en avantage d'une victoire ou le contre d'une victoire de l'adversaire
-    # changer les poids ? -> faire des simulation de quels poids sont plus intéressants (script faisant tourner plusieurs partie, pour des poids différents, pour des temps différents)
-
+    # Poids qui privilegie les lignes de 3
+    weights = {3: 20, 2: 2}
     opponent = 1 - color_player
 
-    # Count pieces of each color in the window
-    my_count = sum(1 for cell in window if cell is not None and cell[1] == color_player)
-    opponent_count = sum(1 for cell in window if cell is not None and cell[1] == opponent)
+    my_count = 0
+    empty_count = 0
+    
+    for cell in window:
+        if cell is not None:
+            if cell[1] == color_player:
+                my_count += 1
+            else:
+                # la fenètre est bloquée par l'adversaire pour la couleur
+                return 0
+        else:
+            empty_count += 1
 
-    # Si un pièce de l'adversaire -> pas de score pour les 4 cases (on peut pas compléter la ligne)
-    if opponent_count > 0:
-        return 0
+    return sign * weights.get(my_count, 0)
 
-    return sign * weights.get(my_count,0) # sign multiplies the value by +1 if it's good for us, -1 if it's good for the opponent
+# Attribue un score à une fenêtre selon le nombre de symboles (X ou O) présents.
+def score_symbol_window(window, symbol):
+    # Les symboles ont moins de poids car tout le monde peut gagner avec
+    weights = {3: 10, 2: 1}
+    count = 0
+    
+    for cell in window:
+        if cell is not None and cell[0] == symbol:
+            count += 1
+            weights.get(count, 0)
+    
+    return weights.get(count, 0)  # le 2eme argument est la si jamais get ne trouve pas la valeur count dans le dico
